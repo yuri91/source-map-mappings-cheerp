2492e82f3aab37c9ea26f5628225f88d3962fd9f
Yuri Iozzelli <y.iozzelli@gmail.com>
2018-05-09 15:32:14 +0200

    Add utility class for nullable numbers

--- utils.h
+++ utils.h
@@ -4,6 +4,52 @@
 #include <string>
 #include <experimental/optional>
 
+#include  <cheerp/client.h>
+#include <type_traits>
+
+template<typename T, typename std::enable_if<std::is_arithmetic<T>::value>::type* = nullptr>
+class [[cheerp::genericjs]] nullable {
+	client::Object* value;
+public:
+	inline nullable() {
+		clear();
+	}
+	inline nullable(T i) {
+		set(i);
+	}
+	inline nullable(client::Object* opaque): value(opaque) {}
+	inline nullable(std::nullptr_t nullp): value(nullptr) {}
+	operator client::Object*() {
+		return get_opaque();
+	}
+	operator bool() {
+		return is_set();
+	}
+	T operator*() {
+		return get();
+	}
+private:
+	inline void set(T i) {
+		__asm__("%0" : "=r"(value) : "r"(i));
+	}
+	inline void clear() {
+		__asm__("null" : "=r"(value));
+	}
+	inline bool is_set() {
+		bool ret;
+		__asm__("%0!==null" : "=r"(ret) : "r"(value));
+		return ret;
+	}
+	inline T get() {
+		T ret;
+		__asm__("%0" : "=r"(ret) : "r"(value));
+		return ret;
+	}
+	inline client::Object* get_opaque() {
+		return value;
+	}
+};
+
 template<class T>
 using optional = std::experimental::optional<T>;
 
